ab2641134a5d911508703ad13c700e10f83a6dad
sunyonghua <525930392@qq.com>
2017-04-26 14:48:59 +0800

    组件更新

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 
-import PersonTitle from '../common/personTitle';
+import PersonTitle from '../common/persontitle';
 
 class Home extends Component{
     render(){
